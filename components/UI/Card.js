98ffdfe3d6a659b1a22731fd55d9ac506ebbf38b
JayBrown92 <49319765+JayBrown92@users.noreply.github.com>
2022-05-05 13:59:15 +0100

    set up reusable card component for list of products.

---
+++ components/UI/Card.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Image from "next/image";
+
+export default function Card({ image, title, price, description }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(!showModal);
+  };
+  return (
+    <>
+      <Image src={image} alt={`${title}`} width={291} height={291} />
+
+      <div className="p-4">
+        <h2 classes={"font-bold"}>{title}</h2>
+        <h3 className="font-italic">£{price}</h3>
+        <p>{description}</p>
+        <button
+          className=" font-bold cursor-pointer py-2.5  "
+          onClick={handleShowModal}
+        >
+          View more
+        </button>
+      </div>
+      <div
+        className={`flex absolute w-full h-full justify-center content-center hidden ${
+          showModal ? "" : "hidden"
+        }`}
+      ></div>
+    </>
+  );
+}
